cb6988bb099801b88e46d1898923050dfe235ea3
Chrisbuildit <115037055+Chrisbuildit@users.noreply.github.com>
2023-01-19 15:24:32 +0100

    Return promise

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
 import './SearchBar.css';
 import axios from "axios";
-import { Hint } from 'react-autocomplete-hint';
+import AsyncSelect from 'react-select/async';
 
 const apiKey = 'Q577X5CIYDHZEQY7';
 
 function SearchBar({ setCompanyHandler }) {
     const [query, setQuery] = useState('');
-    const [searchResults, setSearchResults] = useState([]);
-    const [inputTimer, setInputTimer] = useState(null);
+    const [inputValue, setValue] = useState('');
+
+    // handle input change event
+    const handleInputChange = value => {
+        setValue(value);
+    };
+
+    // handle selection
+    const handleChange = value => {
+        setQuery(value);
+    }
 
     function handleClick() {
         setCompanyHandler(query);
@@ -19,46 +28,35 @@ function SearchBar({ setCompanyHandler }) {
             setCompanyHandler(query);
         }
     }
-    const handleInputChange = async (e) => {
-        setQuery(e.target.value);
-    clearTimeout(inputTimer);
-    let timeout = setTimeout(() => {
+    const fetchData = () => {
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
         console.log("FETCHING RESULTS");
         axios
             .get(
-                `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${e.target.value}&apikey=${apiKey}`
+                `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${inputValue}&apikey=${apiKey}`
             )
             .then((res) => {
-                const hintArray = []
-                res.data.bestMatches.map(a => hintArray.push(a.name))
-                setSearchResults(hintArray);
-                console.log(hintArray);
+                res.data.bestMatches.map(a => a.name)
             });
-    }, 300);
-    setInputTimer(timeout);
-};
+            }, 300);
+    })
+    };
 
     return (
       <>
-        <span>
-          <div className="searchbar">
-              <h5>Try typing these words</h5>
-              <code>{`[${searchResults.toString()}]`}</code>
-              <br/>
-              <br/>
-              <br/>
-              <Hint options={searchResults} allowTabFill>
-                  <input
-                         type="text"
-                         name="search"
+        <span className="searchbar">
+                  <AsyncSelect className='t'
+                         cacheOptions
+                         isMulti
+                         defaultOptions
                          value={query}
-                         onChange={handleInputChange}
+                         onInputChange={handleInputChange}
+                         onChange={handleChange}
                          onKeyUp={keyPressCheck}
                          placeholder="Type the name or symbol of a company"
-                         autoComplete="off"
+                         loadOptions={fetchData}
                   />
-              </Hint>
-          </div>
           <button
               type="button"
               onClick={handleClick}

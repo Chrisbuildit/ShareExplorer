a0df30f50625f76db6ce4ec4ddc7f5efa8260b0f
Chrisbuildit <115037055+Chrisbuildit@users.noreply.github.com>
2023-10-03 09:58:22 +0200

    Implementation of useQuery for SearchBox

--- src/App.js
+++ src/App.js
@@ -9,22 +9,28 @@ import SearchResults from "./pages/searchResults/SearchResults";
 import Profile from "./pages/profile/Profile";
 import CompanyDetails from "./pages/company-details/company-details";
 import SearchPage from "./pages/searchPage/SearchPage";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
 
         return (
-        <>
-            <NavBar />
-            <Routes>
-                    <Route path="/" element={ <Home /> }/>
-                    <Route path="/searchResults/:companyId" element={ <SearchResults /> }/>
-                    <Route path="/company-details/:dateNow" element={ <CompanyDetails /> }/>
-                    <Route path="/profile" element={ <Profile /> }/>
-                    <Route path="/signUp" element={ <SignUp/> }/>
-                    <Route path="/signIn" element={ <SignIn/> }/>
-                    <Route path="/searchPage" element={ <SearchPage/> }/>
-            </Routes>
-        </>
+        // To use React-Query anywhere in the app, I first need to wrap my main component into QueryClientProvider.
+        <QueryClientProvider client={queryClient}>
+            <>
+                <NavBar />
+                <Routes>
+                        <Route path="/" element={ <Home /> }/>
+                        <Route path="/searchResults/:companyId" element={ <SearchResults /> }/>
+                        <Route path="/company-details/:dateNow" element={ <CompanyDetails /> }/>
+                        <Route path="/profile" element={ <Profile /> }/>
+                        <Route path="/signUp" element={ <SignUp/> }/>
+                        <Route path="/signIn" element={ <SignIn/> }/>
+                        <Route path="/searchPage" element={ <SearchPage/> }/>
+                </Routes>
+            </>
+        </QueryClientProvider>
     )
 }
 
--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,117 +1,104 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import './SearchBar.css';
-import axios from "axios";
 import {useNavigate} from "react-router-dom";
-// import { ReactSearchAutocomplete } from 'react-search-autocomplete'
-// import { uuid } from 'uuidv4'
 import Select from 'react-select'
 import OtcStocks from './data/data.json';
+import {useQuery} from '@tanstack/react-query'
+import axios from "axios";
+
+
+const fetchData = async (query) => {
+        console.log("FETCHING RESULTS");
+        const result = await axios.get(
+            `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${query}&apikey=${process.env.REACT_APP_API_KEY}`
+            // `https://yfapi.net/v6/finance/autocomplete?region=US&lang=en&query=${query}`,
+            // 'accept: application/json','X-API-KEY: HyE5hYtoWh5QwPbmpzUVf6sposSaYEqV8BzQd8WD'
+        )
+        console.log(result);
+        const hintArray = []
+        result.data.bestMatches.map((a) => {
+            return hintArray.push({
+                symbol: a["1. symbol"],
+                name: a["2. name"],
+                type: a["3. type"],
+                region: a["4. region"]
+            })
+        });
+        const list = hintArray.filter((post) => {
+            return post.type === "Equity" && post.region === "United States";
+        })
+        // Excludes OTC-shares from list
+        const filteredArray = list.filter((element) =>
+            !OtcStocks.some((otc) => otc.symbol === element.symbol)
+        )
+        return filteredArray.map((share) => {
+            return {label: share.symbol, value: share.name};
+        });
+};
 
 function SearchBar() {
     const [query, setQuery] = useState('');
-    const [searchResults, setSearchResults] = useState([])
-    const [selection, setSelection] = useState()
-    const [error, toggleError] = useState(false)
+    const [selection, setSelection] = useState('')
     const navigate = useNavigate();
-    const [isLoading, setIsLoading] = useState(false)
 
     function handleClick() {
         navigate(`/searchResults/${selection.valueOf().label}`)
-        setQuery("")
     }
 
-    const handleOnSearch = (string) => {
-        setQuery(string);
-        console.log(string)
+    const handleOnSearch = (string, InputActionMeta) => {
+        // if(InputActionMeta.action !== 'input-blur' && InputActionMeta.action !== 'menu-close') {
+            setQuery(string)
+        // }
     }
 
     const handleOnSelect = (value) => {
         setSelection(value)
-        // navigate(`/searchResults/${value.label}`)
-        setSearchResults([])
     }
 
-    const handleOnChange = (selectedOption) => {
-        // setSelection(selectedOption)
-        // console.log(selection);
+    const handleOnChange = () => {
+        setSelection('');
         navigate(`/searchResults`)
     }
 
-    function abort() {
-        setQuery("")
-    }
+    // function abort() {
+    //     setQuery("")
+    // }
 
-    useEffect(() => {
-    const fetchData = async (e) => {
-        toggleError(false);
-        setIsLoading(true);
-        try {
-            console.log("FETCHING RESULTS");
-            const result = await
-                axios.get(
-                    `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${query}&apikey=${process.env.REACT_APP_API_KEY}`
-                    // `https://yfapi.net/v6/finance/autocomplete?region=US&lang=en&query=${query}`,
-                    // 'accept: application/json','X-API-KEY: HyE5hYtoWh5QwPbmpzUVf6sposSaYEqV8BzQd8WD'
-                )
-                console.log(result);
-                const hintArray = []
-                result.data.bestMatches.map((a) => {
-                    return hintArray.push({symbol: a["1. symbol"], name: a["2. name"], type: a["3. type"], region: a["4. region"]})
-                });
-                const list = hintArray.filter((post) => {
-                    return post.type === "Equity" && post.region === "United States";
-                })
-                // Excludes OTC-shares from list
-                const filteredArray = list.filter((element) =>
-                    !OtcStocks.some((otc) => otc.symbol === element.symbol)
-                )
-                setSearchResults(filteredArray);
-                console.log(filteredArray);
-        } catch (e) {
-            console.error(e);
-            toggleError(true);
+    const { isLoading, error, data } = useQuery(
+        query ? ['StockData', query] : ['StockData'],
+        async () => await fetchData(query),
+        {
+            // the duration until a query transitions from fresh to stale.
+            staleTime: 10000,
+            enabled: !!(query.length > 2 && query.length < 10)
         }
-        setIsLoading(false);
-    };
-    if(Object.keys(query).length > 2) {
-    void fetchData();
-    }
-    },[query]);
-
-    const optionList = searchResults.map((share) => {
-        return {label: share.symbol, value: share.name};
-    })
+    )
 
     return (
         <>
     <form className="searchbar" /*onMouseLeave={abort}*/>
         <section>
             <Select
-                // classNamePrefix="mySelect"
-                options={optionList}
+                options={data}
+                // used to set the initial value of the search input
                 inputValue={query}
                 placeholder="Enter the name or symbol of the company"
+                // invoked when there is a change in the search value
                 onInputChange={handleOnSearch}
                 isSearchable={true}
                 isClearable={true}
-                isLoading={isLoading}
+                isLoading={!!query && isLoading}
+                // used to specify a function that filters the option to be displayed from the options prop.
                 filterOption={null}
+                // invoked whenever an option is selected.
                 onChange={handleOnSelect}
-                onFocus={handleOnChange}
                 onMenuOpen={handleOnChange}
+                // specify functions that will be called when the dropdown menu opens
                 value={selection}
                 aria-label="Single select"
                 styles={customStyles}
                 formatOptionLabel={formatOptionLabel}
             />
-            <div className="Warning">
-                  <p>{query.length > 2 ?
-                      <b>Type the symbol of the correct option to perform a search</b>
-                    : query &&
-                      <b>Type a minimum of three characters</b>
-                  }
-                  </p>
-            </div>
             {error &&
                 <p>You have exceeded the search limit of two searches per minute</p>
             }
